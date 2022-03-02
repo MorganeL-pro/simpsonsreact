c366b02f638976847d7f9c6d9a2f066fcaa738ce
Morgane Lailler <morgane.lailler.pro@gmail.com>
2022-03-02 16:04:29 +0100

    quest ok

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './QuoteCard.css';
 
-const QuoteCard = ({ quote, character, image }) => (
+const QuoteCard = ({quote}) => (
   <figure className="QuoteCard">
-    <img src={image} alt={character} />
+    <img src={quote.image} alt={quote.character} />
     <figcaption>
-      <blockquote>{quote}</blockquote>
-      <cite>{character}</cite>
+      <blockquote>{quote.quote}</blockquote>
+      <cite>{quote.character}</cite>
     </figcaption>
   </figure>
 );
